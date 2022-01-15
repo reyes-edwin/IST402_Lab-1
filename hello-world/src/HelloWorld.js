114d83e1c8c8d98242edc05c7e1fa4ca4dc0b85c
Edwin Reyes <reyesedwin2022@gmail.com>
2022-01-15 17:37:35 -0500

    Mimic Lit playground

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -1,37 +1,61 @@
+// Importing template literal tags and the LitElement base class.
 import { html, css, LitElement } from 'lit';
 
+/**
+ * I don't know what the export is doing but here we are creating a class that
+ * would inherit all of the methods and properties from the LitElement.
+ */
 export class HelloWorld extends LitElement {
+  // receives input and store their states as properties. Allows the properties to  update when change.
+  static get properties() {
+    return {
+      introduction: { type: String },
+      planet: { type: String },
+    };
+  }
+
+  // styles elements in the component's shadow root.
   static get styles() {
     return css`
       :host {
-        display: block;
-        padding: 25px;
-        color: var(--hello-world-text-color, #000);
+        display: inline-block;
+        padding: 10px;
+      }
+      .planet {
+        color: var(--planet-color, #00ff6a);
+        font-weight: bold;
       }
     `;
   }
 
-  static get properties() {
-    return {
-      title: { type: String },
-      counter: { type: Number },
-    };
-  }
-
+  // Calling the parent class and initializing the properties.
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
+    this.introduction = 'Hello';
+    this.planet = 'World';
   }
 
-  __increment() {
-    this.counter += 1;
-  }
-
+  /**
+   * Defines the internal DOM. Functions as a template literal.
+   * @returns the object that describes the HTML for Lit to render.
+   */
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <span @click=${this.togglePlanet}
+        >${this.introduction}
+        <span class="planet">${this.planet}</span>
+      </span>
     `;
   }
+
+  // function that's called when a click happens.
+  togglePlanet() {
+    if (this.planet === 'World') {
+      this.planet = 'Neptune';
+    } else {
+      this.planet = 'World';
+    }
+  }
 }
+// defines the custom element we are using and the class it's associated with.
+customElements.define('my-customelement', HelloWorld);
